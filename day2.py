13f8ef1e51742cd5b33a4cf252181dfbe7d9e778
creldy <thisiscreld@gmail.com>
2025-03-04 22:26:26 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,9 @@
+# Tip Calculator Project (Day 2)
+
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+final_bill= bill*(tip/100 +1) / people
+print(f"Amount you have to pay = {round(final_bill, 2)}")
+
