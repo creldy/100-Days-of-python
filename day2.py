35b6e56fb5168acfc9677f2d86a91f0c862d1e3e
creldy <thisiscreld@gmail.com>
2025-03-04 22:49:22 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -5,5 +5,5 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 final_bill= bill*(tip/100 +1) / people
-print(f"Amount you have to pay = {round(final_bill, 2)}")
+print(f"Amount you have to pay = ${round(final_bill, 2)}")
 
