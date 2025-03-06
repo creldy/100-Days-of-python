cdb85d7d9d67f3cc285b6d5b0908b2e08f543cc5
creldy <thisiscreld@gmail.com>
2025-03-06 21:03:56 +0530

    Add files via upload

---
+++ BankerRoulette.py
@@ -0,0 +1,6 @@
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_int= random.randint(0,4)
+# print(random_int)
+print(f"{friends[random_int]} should pay the bill.")
